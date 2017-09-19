085814e813c62f5a9ee6b767c75a1c7d2dea17d0
dasap89 <dmytro.sapotnitskiy@gmail.com>
2017-09-20 00:19:21 +0300

    changes added for deploy

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -146,7 +146,11 @@ SITE_ID = 1
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'post_images')
 
@@ -167,3 +171,4 @@ ACCOUNT_USER_DISPLAY = lambda user: user.email
 
 AUTHENTICATION_BACKENDS = ["account.auth_backends.EmailAuthenticationBackend", ]
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
